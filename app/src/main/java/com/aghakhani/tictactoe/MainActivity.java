a982cb7399919e5242ea9e953e741dc1f3141933
Kiarash Aghakhani <kiarash88ca@gmail.com>
2025-01-15 21:58:49 -0800

    XML design of the application and the new look

--- app/src/main/java/com/aghakhani/tictactoe/MainActivity.java
+++ app/src/main/java/com/aghakhani/tictactoe/MainActivity.java
@@ -1,6 +1,9 @@
 package com.aghakhani.tictactoe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button[][] buttons = new Button[3][3];
+    private boolean playerXTurn = true;
+    private int roundCount = 0;
+    private TextView tvStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,103 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        tvStatus = findViewById(R.id.tv_status);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "btn_" + (i * 3 + j);
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+            }
+        }
+    }
+
+    public void cellClicked(View view) {
+        Button button = (Button) view;
+        if (!button.getText().toString().equals("")) {
+            return;
+        }
+
+        if (playerXTurn) {
+            button.setText("X");
+        } else {
+            button.setText("O");
+        }
+
+        roundCount++;
+
+        if (checkForWin()) {
+            if (playerXTurn) {
+                tvStatus.setText("Player X Wins!");
+            } else {
+                tvStatus.setText("Player O Wins!");
+            }
+            disableButtons();
+        } else if (roundCount == 9) {
+            tvStatus.setText("Draw!");
+        } else {
+            playerXTurn = !playerXTurn;
+            if (playerXTurn) {
+                tvStatus.setText("Player X's Turn");
+            } else {
+                tvStatus.setText("Player O's Turn");
+            }
+        }
+
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void disableButtons() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setEnabled(false);
+            }
+        }
+    }
+
+    public void resetGame(View view) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+
+        playerXTurn = true;
+        roundCount = 0;
+        tvStatus.setText("Player X's Turn");
     }
 }
